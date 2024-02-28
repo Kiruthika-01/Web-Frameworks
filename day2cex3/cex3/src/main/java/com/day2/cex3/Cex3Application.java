1442fadaf6b0790b76d302165aeed557c3b70272
Kiruthika-01 <124856724+Kiruthika-01@users.noreply.github.com>
2024-02-28 21:06:35 +0530

    Add files via upload

---
+++ day2cex3/cex3/src/main/java/com/day2/cex3/Cex3Application.java
@@ -0,0 +1,13 @@
+package com.day2.cex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cex3Application.class, args);
+	}
+
+}
---
+++ day2cex3/cex3/src/test/java/com/day2/cex3/Cex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day2.cex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
